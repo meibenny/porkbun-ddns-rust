0031a6a821166b6a894d4273cc02ffcf222bdf9d
Benny Mei <meibenny@gmail.com>
2023-02-08 00:01:57 -0500

    querying and updating porkbun

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,57 @@
-use clap::Parser;
 use anyhow::{Context, Result};
+use clap::Parser;
+use serde::{Deserialize, Serialize};
 
 #[derive(Parser)]
 struct Cli {
     /// The path to the config file
     config_file: std::path::PathBuf,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ConfigDomainsStruct {
+    domain: String,
+    dns_entry_type: String,
+    subdomain: String
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Config {
+    secretkey: String,
+    apikey: String,
+    domains: Vec<ConfigDomainsStruct>
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct PorkbunUpdateDNSStruct {
+    secretapikey: String,
+    apikey: String,
+    content: String,
+    ttl: String
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct PorkbunQueryDNSRequest {
+    secretapikey: String,
+    apikey: String
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct PorkbunQueryDNSRecord {
+    id: String,
+    name: String,
+    #[serde(rename="type")]
+    type_: String,
+    content: String,
+    ttl: String,
+    prio: String,
+    notes: Option<String>
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct PorkbunQueryDNSResponse {
+    status: String,
+    records: Vec<PorkbunQueryDNSRecord>
 }
 
 fn get_current_ip() -> Result<String, reqwest::Error> {
@@ -15,9 +62,57 @@ fn get_current_ip() -> Result<String, reqwest::Error> {
     Ok(ip)
 }
 
-fn update_dns_entry(current_ip: String) {
+fn get_current_dns_entry(config: &Config) -> Result<String, reqwest::Error> {
+    let ref secretkey = config.secretkey;
+    let ref apikey = config.apikey;
+    let ref domain_config = config.domains[0];
+    let porkbun_payload = PorkbunQueryDNSRequest {
+        secretapikey: String::from(secretkey),
+        apikey: String::from(apikey)
+    };
+    let url = &format!(
+        "https://porkbun.com/api/json/v3/dns/retrieveByNameType/{}/{}/{}",
+        domain_config.domain, domain_config.dns_entry_type, domain_config.subdomain
+    );
+    let client = reqwest::blocking::Client::new();
+    let resp = client.post(url)
+        .json(&porkbun_payload)
+        .send()?;
+    //println!("{}", &resp.text()?);
+    let parsed_resp = resp.json::<PorkbunQueryDNSResponse>()?;
+    println!("{:?}", parsed_resp);
+    Ok(parsed_resp.records[0].content.to_string())
+}
+
+fn update_dns_entry(
+    current_ip: &String,
+    current_dns_entry: &String,
+    config: &Config
+) -> Result<String, reqwest::Error> {
     println!("The current ip is: {}", current_ip);
     println!("{}", "updating dns entry");
+    let ref secretkey = config.secretkey;
+    let ref apikey = config.apikey;
+    let ref domain_config = config.domains[0];
+    let url = &format!(
+        "https://porkbun.com/api/json/v3/dns/editByNameType/{}/{}/{}",
+        domain_config.domain, domain_config.dns_entry_type, domain_config.subdomain
+    );
+    println!("{}", url);
+    println!("{} {}", secretkey, apikey);
+    let porkbun_payload = PorkbunUpdateDNSStruct {
+        secretapikey: String::from(secretkey),
+        apikey: String::from(apikey),
+        content: String::from(current_ip),
+        ttl: String::from("600")
+    };
+    //println!("{:?}", porkbun_payload);
+    let client = reqwest::blocking::Client::new();
+    let resp = client.post(url)
+        .json(&porkbun_payload)
+        .send()?;
+    println!("{}", resp.text()?);
+    Ok("Ok".to_string())
 }
 
 fn main() -> Result<()> {
@@ -29,9 +124,9 @@ fn main() -> Result<()> {
     for line in config_contents.lines() {
         println!("{}", line);
     }
-    let parsed_config = json::parse(&config_contents).unwrap();
-    let secretkey = &parsed_config["secretkey"];
-    println!("{}", parsed_config);
+    let parsed_config: Config = serde_json::from_str(&config_contents).unwrap();
+    let ref secretkey = parsed_config.secretkey;
+    println!("{:?}", parsed_config);
     println!("{}", secretkey);
     let current_ip_result = get_current_ip();
     let current_ip = match current_ip_result {
@@ -39,6 +134,7 @@ fn main() -> Result<()> {
        Err(error) => panic!("Could not retrieve current IP: {:?}", error),
     };
     println!("{}", current_ip);
-    update_dns_entry(current_ip);
+    let current_entry = get_current_dns_entry(&parsed_config).unwrap();
+    let update_result = update_dns_entry(&current_ip, &current_entry, &parsed_config).unwrap();
     Ok(())
 }
